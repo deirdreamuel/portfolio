1eaee59463bc4bb363c94e65ef29302e0c62be73
xerzen <xerzen@live.com>
2019-10-15 09:44:01 -0700

    "Added various sorting options, speed, and play/pause buttons"

--- src/main_page.js
+++ src/main_page.js
@@ -4,54 +4,105 @@ import { BrowserRouter as Router, Link} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import Canvas from './sketch';
 
-function MainPage(props) {
-    return (
-        <Router>
-            <div className = "container-1">
-                <div className = "NameTab">
-                    <h1 className = "firstLine">   
-                        <a href = "/" className ="topcenter"> {props.name} </a>
-                        
-                    </h1>
+class MainPage extends React.Component  {
+    constructor(props) {
+        super(props)
+        this.state = {value:'1', speed:'1', active: 0, reset: 0}
+    }
+
+    activate = e => {
+        this.setState({active : 1});
+    }
+
+    deactivate = e => {
+        this.setState({active : 0});
+    }
+
+    changeAlg = e => { 
+        this.setState({value : e.target.value});
+    }
+
+    changeSpeed = e => {
+        this.setState({speed : e.target.value});
+    }
+    
+    shuffle = e => {
+        this.setState({reset : 1});
+    }
+
+    render() {    
+        return (
+            <Router>
+                <div className = "container-1">
+                    <div className = "NameTab">
+                        <h1 className = "firstLine">   
+                            <a href = "/" className ="topcenter"> {this.props.name} </a>
+                            
+                        </h1>
+                    </div>
+
+                    <div className = "ProjectsTab">
+                        <h1 className = "firstLine">
+                            <a href = "sort" className = "topright"> SORT </a> 
+                        </h1>
+                    </div>
+
+                    <div className = "ResumeTab">
+                        <h1 className = "firstLine">
+                            <a href = "Resume" className = "topleft"> RESUME </a>
+                        </h1>
+                    </div>
+
+                    <Route path = "/" exact strict render= {
+                        ()=> {
+                            return (<p></p>);
+                        }
+                    }/>
+
+                    <Route path = "/sort" exact strict render= {
+                        ()=> {
+                            return (
+                                <div>                                  
+                                    <div>    
+                                        <Canvas value={this.state.value} speed = {this.state.speed} active = {this.state.active} reset = {this.state.reset}/>
+                                    </div>
+
+                                    <div className = "drop_down">
+                                        <select value = {this.state.value} onChange = {this.changeAlg}>
+                                            <option value='1'>Bubble Sort</option>
+                                            <option value='2'>Selection Sort</option>
+                                            <option value='3'>Insertion Sort</option>
+                                            <option value='4'>Quick Sort</option>
+                                        </select>
+
+                                        <select value = {this.state.speed} onChange = {this.changeSpeed}>
+                                            <option value='1'>1x</option>
+                                            <option value='2'>2x</option>
+                                            <option value='3'>3x</option>
+                                            <option value='4'>4x</option>
+                                        </select>                                       
+                                    </div> 
+
+                                    <div className = "button">
+                                        <button type="button" onClick = {this.activate}> PLAY </button>
+                                        <button type="button" onClick = {this.deactivate}> PAUSE </button>
+                                        <button type="shuffle" onClick = {this.shuffle}> SHUFFLE </button>
+                                    </div>    
+                                </div>
+                            );
+                        }
+                    }/>
+
+
                 </div>
+                
 
-                <div className = "ProjectsTab">
-                    <h1 className = "firstLine">
-                        <a href = "sort" className = "topright"> SORT </a> 
-                    </h1>
-                </div>
-
-                <div className = "ResumeTab">
-                    <h1 className = "firstLine">
-                        <a href = "Resume" className = "topleft"> RESUME </a>
-                    </h1>
-                </div>
-
-                <Route path = "/" exact strict render= {
-                    ()=> {
-                        return (<p></p>);
-                    }
-                }/>
-
-                <Route path = "/sort" exact strict render= {
-                    ()=> {
-                        return (
-                            <div>
-                                
-                                <Canvas />
-                                
-                            </div>
-                        );
-                    }
-                }/>
-
-
-            </div>
+            </Router>
             
+        );
+    }
 
-        </Router>
-        
-    );
+    
 }
 
 export default MainPage;
--- src/sketch.js
+++ src/sketch.js
@@ -1,60 +1,111 @@
 import React, { Component } from 'react';
 
-let values = [];
+let size = 300;
+let v = '1'
+let s = '1'
+
+let values = []; // get ascending numbers.
+for (let i = 0; i <= size; ++i)
+  values [i] = i;
+    
 
 class Canvas extends Component {
   componentDidMount() {
+    // shuffle numbers if 'randomize' when initialized
+    shuffle(values);
+    this.draw(values);
+
+    let id = window.setInterval(() => {
+    // draw the canvas ( style: -scatter_plot, -bargraph, lines)
+      this.display(values);
+    }, 200);
+
+  }
+
+  clear(arr) {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
-
-    //Get ascending numbers
-    for (let i = 0; i <= 300; ++i) {
-      values [i] = i;
-    }
-
-    //shuffle numbers
-    for (let i = 0; i < 300 ; i++)
-    {
-        let rand_num = Math.floor(Math.random() * i);
-        swap(values, rand_num, i);
-    }
-
-    for (let i = 0; i <= 300; i += 5) {
-      ctx.beginPath();
-      ctx.rect(i, 300 , 5, -values[i]);
-      ctx.fillStyle = "red";
-      ctx.fill();
-      ctx.stroke();
-    } 
-    
-    this.updateAnimation();
-  
+    ctx.clearRect(0, 0, size, size);
   }
 
-  updateAnimation(){
-    window.setInterval(() => {
-      selection_sort(values, 300);
+  done(f,id) {
+    if(!this.props.active) {
+      clearInterval(id);
+    }
 
+    if (s != this.props.speed) {
+      s = this.props.speed;
+      clearInterval(id);
+      this.display(values);
+    }
+    
+    if(f.index > size*size) //end when sorting is done    
+      clearInterval(id);
+  }
+
+  draw(arr) {
+    
       const canvas = this.refs.canvas;
       const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, 300, 300);
-
-      for (let i = 0; i <= 300; i += 5) {
-      ctx.beginPath();
-      ctx.rect(i, 300 , 5, -values[i]);
-      ctx.fillStyle = "red";
-      ctx.fill();
-      ctx.stroke();
-      } 
-
-    },50);
     
+      ctx.clearRect(0, 0, size, size);
+
+      for (let i = 0; i <= size; i += 5) {
+        ctx.beginPath();
+        ctx.rect(i,size-arr[i], 5, 5);
+        ctx.fillStyle = "red";
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "red";
+        ctx.fill();
+        ctx.stroke();
+      }
   }
-  
+
+  display(arr) { // display sorting algorithm of choice 
+    var iteration =  {index : 0};
+
+    if (this.props.active) {
+      let id = window.setInterval(() => {
+        
+        this.done(iteration, id);
+        this.iteration(arr, iteration);
+        this.draw(arr); 
+      },this.interval()); //set interval
+    }
+  }
+
+  interval () {
+    switch(this.props.speed) {
+      case '1': return 100;
+      case '2': return 50; 
+      case '3': return 25; 
+      case '4': return 12; 
+    }
+  }
+
+  iteration(arr, f) {
+    if (this.props.reset) {
+      shuffle(arr);
+      f.index = 0;
+    }
+
+    if (v != this.props.value) {
+      f.index = 0;
+      v = this.props.value;
+    }
+    
+    switch (v) {
+      case '1': bubble_sort(arr, size, f.index++); break;
+      case '2': selection_sort(arr, size, f.index++); break;
+      case '3': insertion_sort(arr, size, f.index++); break;
+      //case '4': quick_sort(arr, size, f.index++); break;
+    }
+  }
+
   render() {
     return(
       <div>
-        <canvas ref="canvas" width={300} height={300} />
+        <canvas ref="canvas" width={size} height={size} />
       </div>
     );
   }
@@ -63,44 +114,51 @@ class Canvas extends Component {
 
 export default Canvas;
 
-function bubble_sort(arr, arr_size) {
-  for (let i = arr_size; i >= 0; --i) {
-    let swapped = false;
-    for (let j = 1; j < i; ++j) {
-      if (values[j-1] > values[j]) {
-        swap(values, j-1, j);
-        swapped = true;
-      }
-    }
-    return swapped;
+//Fisher-Yates shuffle function
+function shuffle (arr) {
+  for (let i = 0; i < size ; i++) {
+      let r = Math.floor(Math.random() * i);
+      swap(arr, r, i);
   }
 }
 
-function selection_sort(arr, arr_size) {
-  for (let i = 0; i < arr_size-1; ++i) {
-    let min = i;
+
+
+
+//-----   Sorting algorithms -----//
+function bubble_sort(arr, arr_size, i) {   
+    for (let j = 1; j < arr_size; ++j) {
+      if (arr[j-1] > arr[j]) 
+        swap(arr, j-1, j);
+
+  }
+} //-----
+
+function selection_sort(arr, arr_size, i) {
+    var min = i;
+
     for (let j = i + 1; j < arr_size; ++j) {
       if (arr[j] < arr[min]) {
         min = j;
       }
     }
-    if (min != i) {
-      swap(arr, i, min);
-      return true;
-    }
-  }  
-}
 
-function insertion_sort(arr, arr_size) {
-  for (let i = 1; i < arr_size; ++i) {
+    if (min !== i) 
+      swap(arr, i, min);  
+} //-----
+
+
+function insertion_sort(arr, arr_size, i) {
+    if (i == 0) 
+      i = 1;
     for (let j = i; j > 0; --j) {
       if(arr[j] < arr[j-1]){
         swap(arr, j, j-1);
       }
     } 
-  }
 }
 
+//swap function ( [], index a, b)
 function swap(arr, a, b) {
   let tmp = arr[a];
   arr[a] = arr[b];
